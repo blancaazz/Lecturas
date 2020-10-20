3af9e529eac3176eb32d7a62edbe64878457054b
Blanca Luque <blanluja@gmail.com>
2020-10-20 16:38:56 +0200

    Realizados tests para mostrar informacion #5

--- test/Registro_test.js
+++ test/Registro_test.js
@@ -8,6 +8,8 @@ const datos_momo = "Momo - Michael Ende - Me ha gustado mucho - 8";
 const libro_habitacion = new Libro("Una habitación propia", "Virginia Woolf", "Me ha parecido muy interesante", 8);
 const datos_habitacion = "Una habitación propia - Virginia Woolf - Me ha parecido muy interesante - 8";
 
+const nombre_momo = "Momo";
+const nombre_habitacion = "Una habitación propia"
 
 var registro = new Registro_libro();
 const string_prueba = "Libro de la selva"
@@ -40,12 +42,24 @@ describe("Registro libro", function(){
         it("Debería egistrar más de un libro", function(){
             registro.registrarLibro(libro_habitacion);
             assert.equal(registro.getUltimoLibro(), datos_habitacion, "Registrado");
-        })
+        });
         it("No debería registrar el libro", function(){
             registro.registrarLibro(string_prueba);
                 assert.notEqual(registro.getUltimoLibro(), string_prueba, "No registrado");
         });
     });
 
-    
+    //Corresponde a la historia de usuaria 2: mostrar información sobre un libro (dado su nombre)
+    describe("Mostrar información de un libro (HU2)", function(){
+        it("Debería mostrar la informacion del libro 1", function(){
+            assert.equal(registro.mostrarInformacion(nombre_momo), datos_momo, "Mostrada correctamente");
+        });
+        it("Debería mostrar la informacion del libro 2", function(){
+            assert.equal(registro.mostrarInformacion(nombre_habitacion), datos_habitacion, "Mostrada correctamente");
+        });
+        it("No debería mostrar la información", function(){
+            assert.notEqual(registro.mostrarInformacion(string_prueba), string_prueba, "No mostrada");
+        });
+    });
+
 });
