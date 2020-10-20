ba83e7ab9ef4a83237d223faecb0f2cd87ab19fe
Blanca Luque <blanluja@gmail.com>
2020-10-20 13:35:08 +0200

    Realizado test de registro #4

--- test/Registro_test.js
+++ test/Registro_test.js
@@ -3,6 +3,15 @@ var Libro = require("../src/Libro.js");
 const assert = require("assert");
 const { isMainThread } = require("worker_threads");
 
+const libro_momo = new Libro("Momo", "Michael Ende", "Me ha gustado mucho", 8);
+const datos_momo = "Momo - Michael Ende - Me ha gustado mucho - 8";
+const libro_habitacion = new Libro("Una habitación propia", "Virginia Woolf", "Me ha parecido muy interesante", 8);
+const datos_habitacion = "Una habitación propia - Virginia Woolf - Me ha parecido muy interesante - 8";
+
+
+var registro = new Registro_libro();
+const string_prueba = "Libro de la selva"
+
 describe("Registro libro", function(){
     describe("Carga", function(){
         it("Debería cargarse", function(){
@@ -14,9 +23,29 @@ describe("Registro libro", function(){
     describe("Crea", function(){
         it("Debería crear un registro", function(){
             var nuevo_registro = new Registro_libro();
-            var libro = new Libro("Momo", "Michael Ende", "Me ha gustado mucho", 8);
-            nuevo_registro.registrarLibro(libro);
-            assert.equal(nuevo_registro.mostrarInformacion("Momo"), "Momo - Michael Ende - Me ha gustado mucho - 8", "Creado");
+            assert.equal(nuevo_registro.as_string(),"" , "Creado");
         });
     });
+
+    //Corresponde a la historia de usuario 1: registrar libro
+    describe("Registrar libro (HU1)", function(){
+        it("Debería registrar un libro", function(){
+            registro.registrarLibro(libro_momo);
+            assert.equal(registro.as_string(), datos_momo , "Registrado");
+        });
+        it("No debería registrar un libro", function(){
+            registro.registrarLibro(string_prueba)
+            assert.notEqual(registro.as_string(), string_prueba, "No registrado");
+        });
+        it("Debería egistrar más de un libro", function(){
+            registro.registrarLibro(libro_habitacion);
+            assert.equal(registro.getUltimoLibro(), datos_habitacion, "Registrado");
+        })
+        it("No debería registrar el libro", function(){
+            registro.registrarLibro(string_prueba);
+                assert.notEqual(registro.getUltimoLibro(), string_prueba, "No registrado");
+        });
+    });
+
+    
 });
