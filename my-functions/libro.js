2f05502e698beaea880c9ceeeac750f7a2bbccb0
Blanca Luque <blanluja@gmail.com>
2020-12-01 23:21:07 +0100

    Probando

---
+++ my-functions/libro.js
@@ -0,0 +1,22 @@
+const Registro_libros = require("../src/Registro_libros.js");
+const datos = require("./datos.js")
+
+
+exports.handler = async function(event, context){
+    var nombre = event.queryStringParameters.nombre;
+    console.log(nombre);
+    registro = datos.crearRegistro();
+    var libro = registro.getLibro(nombre);
+    var contenido;
+    if(libro != undefined){
+        contenido = JSON.stringify(contenido);
+    }
+    else{
+        contenido = JSON.stringify({error: "Fallo al buscar libro"});
+    }
+    return {        
+        statusCode: 200,
+        body: contenido
+    }; 
+    
+}
