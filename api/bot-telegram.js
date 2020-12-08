ff3ebee05316308bac02179328aa95568e7a5847
Blanca Luque <blanluja@gmail.com>
2020-12-08 10:57:46 +0100

    Añadiendo bot-telegram.js

---
+++ api/bot-telegram.js
@@ -0,0 +1,13 @@
+
+
+
+module.exports = (req, res) => {
+    registro = datos.crearRegistro();
+    //var nombre = req.query.nombre;
+    //var libro = registro.getLibro(nombre);
+    if (libro == undefined){
+        res.status(404).send({error: "No se ha introducido correctamente"})
+    }
+    //res.status(200).send(libro); 
+    res.send("holi")   
+}
