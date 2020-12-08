0fe37562a6e2d8317a672b8aea605422dc603094
Blanca Luque <blanluja@gmail.com>
2020-12-08 11:01:15 +0100

    Modifico bot-telegram.js

--- api/bot-telegram.js
+++ api/bot-telegram.js
@@ -2,12 +2,12 @@
 
 
 module.exports = (req, res) => {
-    registro = datos.crearRegistro();
+    //registro = datos.crearRegistro();
     //var nombre = req.query.nombre;
     //var libro = registro.getLibro(nombre);
-    if (libro == undefined){
-        res.status(404).send({error: "No se ha introducido correctamente"})
-    }
+    //if (libro == undefined){
+    //    res.status(404).send({error: "No se ha introducido correctamente"})
+    //}
     //res.status(200).send(libro); 
     res.send("holi")   
 }
