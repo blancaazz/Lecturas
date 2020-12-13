bfae4305a456c8712ee1b9c4ae6573010871acc0
Blanca Luque <blanluja@gmail.com>
2020-12-13 20:20:50 +0100

    Desplegando bot de telegram con vercel #27

--- api/bot-telegram.js
+++ api/bot-telegram.js
@@ -1,13 +1,89 @@
+const datos = require("./datos.js")
+const axios = require("axios");
 
 
+function getBaseUrl(){
+    return "https://api.telegram.org/bot" + process.env.TOKEN_TELEGRAM;
+}
+
+var TOKEN = "1443881115:AAF2OJ6h-Yoizh998eAsNH9Rr2n6KPPFkiY";
 
 module.exports = (req, res) => {
-    //registro = datos.crearRegistro();
-    //var nombre = req.query.nombre;
-    //var libro = registro.getLibro(nombre);
-    //if (libro == undefined){
-    //    res.status(404).send({error: "No se ha introducido correctamente"})
-    //}
-    //res.status(200).send(libro); 
-    res.send("holi")   
+    var chat_id = req.body["message"]["chat"]["id"]
+    var texto = req.body["message"]["text"]
+    console.log(chat_id);
+    console.log(texto);
+
+    if(texto == "/hola"){
+        axios.post("https://api.telegram.org/bot" + TOKEN + "/sendMessage",
+        //axios.post("https://api.telegram.org/bot" + process.env.TOKEN_TELEGRAM + "/sendMessage",
+            {
+                chat_id: chat_id,
+                text: "Hola!!"
+            })
+            .then((response) => {
+                console.log("Mandando respuesta");
+                res.status(200).send(response);
+            }).catch((error) => {
+        //     console.log(error);
+                console.log("Error al mandar respuesta")
+                res.send(error);
+            });
+        }
+    else if(texto == "/listalibros"){
+        registro = datos.crearRegistro();
+        axios.post("https://api.telegram.org/bot" + TOKEN + "/sendMessage", 
+        //axios.post("https://api.telegram.org/bot" + process.env.TOKEN_TELEGRAM + "/sendMessage", registro)
+            {
+                chat_id: chat_id,
+                text: JSON.stringify(registro)
+            })
+            .then((response) => {
+                console.log("Mandando respuesta");
+                res.status(200).send(response);
+            }).catch((error) => {
+             console.log(error);
+                console.log("Error al mandar respuesta")
+                res.send(error);
+            });
+        }
+    else if(texto == "/libro"){
+        registro = datos.crearRegistro();
+        axios.post("https://api.telegram.org/bot" + TOKEN + "/sendMessage", 
+        //axios.post("https://api.telegram.org/bot" + process.env.TOKEN_TELEGRAM + "/sendMessage", registro)
+            {
+                chat_id: chat_id,
+                text: "lista de libros",
+                reply_markup: [
+                    {
+                        text: "holi",
+                        callback_data : ""
+                    },
+                    {
+                        text: "hadios",
+                        callback_data: ""
+                    }
+                ]
+            })
+            .then((response) => {
+                console.log("Mandando respuesta");
+                res.status(200).send(response);
+            }).catch((error) => {
+             console.log(error);
+                console.log(error);
+                console.log("Error al mandar respuesta")
+                res.send(error);
+            });
+    }
+    else if(texto == "/help"){
+
+    }
+    else{
+        res.status(200).send({})
+    }
+
+    // var data = {Msg: text,
+    //     Method: "sendMesage",
+    //     ChatID: chat_id}
+    // res.status(200).send(JSON.stringify(data))
 }
