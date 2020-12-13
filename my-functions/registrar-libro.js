b0958e009ddba310e620968bad225c5a94726b2a
Blanca Luque <blanluja@gmail.com>
2020-12-13 20:22:26 +0100

    Añadida funcionalidad de insertar libro netlify #21

--- my-functions/registrar-libro.js
+++ my-functions/registrar-libro.js
@@ -1,14 +1,31 @@
 const Registro_libros = require("../src/Registro_libros.js");
+const Libro = require("../src/Libro");
 const datos = require("./datos.js")
 
+const data = require("./data.js");
+
+const libro_ceguera = new Libro("Ensayo sobre la ceguera", "Saramago", "Ha sido una lectura interesante y reflexiva", 8);
 
 exports.handler = async function(event, context){
-    var nombre = event.queryStringParameters.nombre;
-    console.log(nombre);
-    registro = datos.crearRegistro();
-    return {        
-        statusCode: 200,
-        body: "creado correctamente"
-    }; 
+
+    var registro = new Registro_libros;
     
+    for (i in data.data.libros){
+        const libro = data.data.libros[i]
+        const nombre = libro.nombre;
+        const autora = libro.autora;
+        const comentario = libro.comentario;
+        const puntuacion = libro.puntuacion;
+        registro.registrarLibro(new Libro(nombre, autora, comentario, puntuacion));        
+    }
+
+    //registramos ahora el libro que queremos:
+    registro.registrarLibro(libro_ceguera);
+
+    //guardamos el registro
+    data.data = registro;
+    return{
+        statusCode: 200,
+        body: JSON.stringify({"mensaje": "Se ha agregado correctamente"})
+    }
 }
