b248d6fa99099a080b87decd5ee94be678220074
Blanca Luque <blanluja@gmail.com>
2020-12-13 20:49:17 +0100

    Añadiendo parte frontend #26

---
+++ my-functions/index.js
@@ -0,0 +1,101 @@
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+
+    if ("withCredentials" in xhr) {
+  
+      // Check if the XMLHttpRequest object has a "withCredentials" property.
+      // "withCredentials" only exists on XMLHTTPRequest2 objects.
+      xhr.open(method, url, true);
+  
+    } else if (typeof XDomainRequest != "undefined") {
+  
+      // Otherwise, check if XDomainRequest.
+      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+  
+    } else {
+  
+      // Otherwise, CORS is not supported by the browser.
+      xhr = null;
+  
+    }
+    
+    return xhr;
+
+}
+
+
+$(document).ready(function(){
+     
+    var url = '/.netlify/functions/lista-recomendados';
+    var url2 = "/.netlify/functions/lista-recomendados?genero=list"
+
+    var xhttp = createCORSRequest('GET', url);
+    if (!xhttp) {
+        throw new Error('CORS not supported');
+    }
+  
+    xhttp.onload = function() {
+      
+        var text = xhttp.responseText;
+      
+  
+        var myJSON = text;
+        $("#Lista").text(myJSON);
+
+    };
+
+    //pedimos la lista de géneros
+    var xhttp2 = createCORSRequest('GET', url2);
+    if (!xhttp2) {
+        throw new Error('CORS not supported');
+    }
+  
+    xhttp2.onload = function() {
+      
+        var texto = xhttp2.responseText;
+        var lista = texto.split(",");
+        var option = "";
+        for(i in lista){        
+            option += "<option>" + lista[i] + "</option>";
+        }
+        $("#inputGenero").append(option);
+
+    };
+  
+    xhttp.send();
+  
+    xhttp2.send();
+
+
+
+
+
+    $("#gbtn").click(function(){
+
+        var elemento = $('#inputGenero').find(":selected").text()
+        console.log("Button clicked");
+        console.log(elemento);
+        var url = "/.netlify/functions/lista-recomendados/?genero=" + elemento;
+
+        var xhttp = createCORSRequest('GET', url);
+        if (!xhttp) {
+            throw new Error('CORS not supported');
+        }
+      
+        xhttp.onload = function() {
+          
+            var text = xhttp.responseText;
+
+            $("#Lista").text(text);
+    
+        };
+        xhttp.send();
+    
+      });
+
+  
+  });
+
