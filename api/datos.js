b2d019007a0c69630a3da95a8d6d3619a304652d
Blanca Luque <blanluja@gmail.com>
2020-12-14 23:54:40 +0100

    Añadiendo libro data.js de Vercel

--- api/datos.js
+++ api/datos.js
@@ -4,17 +4,23 @@ const Libro = require("../src/Libro.js");
 function crearRegistro(){
     const libro_momo = new Libro("Momo", "Michael Ende", "Me ha gustado mucho", 8);
     const datos_momo = "Momo - Michael Ende - Me ha gustado mucho - 8";
+   
     const libro_habitacion = new Libro("Una habitación propia", "Virginia Woolf", "Me ha parecido muy interesante", 8);
     const datos_habitacion = "Una habitación propia - Virginia Woolf - Me ha parecido muy interesante - 8";
+   
+    const libro_ceguera = new Libro("Ensayo sobre la ceguera", "Saramago", "Ha sido una lectura interesante y reflexiva", 9);
+
 
     const nombre_momo = "Momo";
     const nombre_habitacion = "Una habitación propia";
+    const nombre_ceguera = "Ensayo sobre la ceguera"
 
     var registro = new Registro_libros();
 
 
     registro.registrarLibro(libro_momo);
     registro.registrarLibro(libro_habitacion);
+    registro.registrarLibro(libro_ceguera);
     return registro;
 }
 
